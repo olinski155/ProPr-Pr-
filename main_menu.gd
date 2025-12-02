3ca3b64e083a3c5318bcd8edd2ed7f0b281c7fc7
tjorvenerikalmes-bit <tjorvenerikalmes@gmail.com>
2025-12-02 15:29:23 +0100

    main menu

---
+++ main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print("Start pressed")
+	
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+
+func _on_Einstellungen_2_pressed() -> void:
+	get_tree().quit()
+	
+	
