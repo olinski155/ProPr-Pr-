f46b29860f579d0fd4d99c21db305fe543dba471
tjorven <tjorvenerikalmes@gmail.com>
2025-12-24 17:45:22 +0100

    fix all the stuff

--- main_menu.gd
+++ main_menu.gd
@@ -2,17 +2,15 @@ extends Control
 
 @onready var main_buttons: VBoxContainer =$"Main Buttons"
 @onready var Einstellungen: Panel =$Options
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	print("Start pressed")
 	
-	func _ready():
+func _ready():
 	main_buttons.visible = true
-	Einstellugen.visible = false
+	Einstellungen.visible = false
 
 
 func _on_start_pressed() -> void:
@@ -21,10 +19,10 @@ func _on_start_pressed() -> void:
 func _on_Verlassen_3_pressed() -> void:
 	get_tree().quit()
 	
-	func _on_Einstellungen_pressed():
+func _on_Einstellungen_pressed():
 	print("Einstellungen pressed")
 	main_buttons.visible = false
-	Einstellugen.visible = true
+	Einstellungen.visible = true
 
 
 func _on_zurück_Einstellungen_pressed() -> void:
