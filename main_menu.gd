d1f865cbb2e20ad6077333353728075a4ec59354
tjorven <tjorvenerikalmes@gmail.com>
2026-01-02 17:37:05 +0100

    main menu audio settings
    Erst die hälfte

--- main_menu.gd
+++ main_menu.gd
@@ -1,15 +1,19 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $"Main Buttons"
+@onready var Einstellungen: Panel = $Einstellungen
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _ready():
+	main_buttons.visible = true
+	Einstellungen.visible = false
+	
 
 func _on_Start_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/level_1.tscn")
@@ -17,7 +21,14 @@ func _on_Start_pressed() -> void:
 
 func _on_Einstellungen_2_pressed() -> void:
 	print("Einstellungen pressed")
+	main_buttons.visible = false
+	Einstellungen.visible = true
 
 
 func _on_Verlassen_3_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_zurück_Einstellungen_pressed() -> void:
+	_ready()
+	
