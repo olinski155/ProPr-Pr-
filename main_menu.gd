e7fefbebe0b2aa59fcb911799160408ec437b50f
tjorven <tjorvenerikalmes@gmail.com>
2026-01-02 14:38:05 +0100

    New main menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,29 +1,23 @@
 extends Control
 
-@onready var main_buttons: VBoxContainer =$"Main Buttons"
-@onready var Einstellungen: Panel =$Options
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("Start pressed")
-	
-func _ready():
-	main_buttons.visible = true
-	Einstellungen.visible = false
+	pass
 
 
-func _on_start_pressed() -> void:
+func _on_Start_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+
+
+func _on_Einstellungen_2_pressed() -> void:
+	print("Einstellungen pressed")
+
 
 func _on_Verlassen_3_pressed() -> void:
 	get_tree().quit()
-	
-func _on_Einstellungen_pressed():
-	print("Einstellungen pressed")
-	main_buttons.visible = false
-	Einstellungen.visible = true
-
-
-func _on_zurück_Einstellungen_pressed() -> void:
-	_ready()
