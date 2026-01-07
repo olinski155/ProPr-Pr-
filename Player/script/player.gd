1917d6be5a34f6bfae008060380464b104218267
olinski155 <147331155+olinski155@users.noreply.github.com>
2026-01-07 17:59:52 +0100

    ich hab versucht urias problem zu fixen
    ging nicht noch fixen bevor neues vid

--- Player/script/player.gd
+++ Player/script/player.gd
@@ -15,9 +15,9 @@ var movement = Vector2()
 @export var wall_slide = 10
 @onready var left_ray: RayCast2D = $raycast/left_ray
 @onready var right_ray: RayCast2D = $raycast/right_ray
-@export var wall_x_force =200.0
+@export var wall_x_force = 200.0
 @export var wall_y_force = -220.0
-var is_wall_jumping =false
+var is_wall_jumping = false
 
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
@@ -38,8 +38,8 @@ func horizontal_movement():
 	if movement:
 		velocity.x = movement * move_speed
 	else:
-			velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
-			
+		velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
+
 func set_animation():
 	if velocity.x != 0:
 		$anim.play("move")
@@ -68,29 +68,9 @@ func jump_logic():
 		jump_amount -= 1
 		velocity.y -= lerp(jump_speed, acceleration, 0.1)
 		velocity.y = 0.3		
-		
 	else:
 		return
-		
+
 func wall_logic():
-	if is_on_wall_only(): 
-		var _wall_slide_speed = 60.0
-		if velocity.y >  _wall_slide_speed:
-			velocity.y = _wall_slide_speed
-		
-		
-		
-		if Input.is_action_just_pressed("ui_accept"):
-			if left_ray.is_colliding():
-				velocity = Vector2(wall_x_force, wall_y_force)
-				wall_jumping()
-			if right_ray.is_colliding():
-				jump_amount = 2 
-				velocity = Vector2(-wall_x_force, wall_y_force)
-				wall_jumping()
-				
-func wall_jumping():
-	is_wall_jumping = true
-	await get_tree().create_timer(0.12).timeout
-	is_wall_jumping = false
-				
+	if is_on_wall_only():
+		velocity.y = 10
