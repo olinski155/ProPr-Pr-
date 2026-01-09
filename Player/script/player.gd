95587f36cc7bef87e84e359463ce5c41d6b87f29
CrabbyShelf <enzo@zwanzig.email>
2026-01-09 14:23:34 +0100

    neyga

--- Player/script/player.gd
+++ Player/script/player.gd
@@ -19,9 +19,21 @@ var movement = Vector2()
 @export var wall_y_force = -220.0
 var is_wall_jumping = false
 
+@export_category("Dash variable")
+@export var dash_speed = 400.0
+@export var facing_right = true
+@export var dashgravity_ = 0
+@export var dash_number = 1
+var dash_key_pressed = 0
+var is_dashing = false
+var dash_timer = Timer
+
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+	if is_dashing ==false:
+		velocity.y += gravity * delta
+	elif is_dashing == true:
+		velocity.y = dashgravity_
 	
 	horizontal_movement()
 	jump_logic()
@@ -32,14 +44,18 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func horizontal_movement():
-	if is_wall_jumping == false:
+	if is_wall_jumping == false and is_dashing == false:
 		movement = Input.get_axis("ui_left", "ui_right")
 	
 	if movement:
 		velocity.x = movement * move_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
-
+	if Input.is_action_just_pressed("ui_dash") and dash_key_pressed == 0 and dash_number>= 1:
+		dash_number -= 1
+		dash_key_pressed = 1
+		dash()
+		
 func set_animation():
 	if velocity.x != 0:
 		$anim.play("move")
@@ -54,16 +70,17 @@ func set_animation():
 
 func flip():
 	if velocity.x > 0.0:
+		facing_right = true
 		scale.x = scale.y * 1
 		wall_x_force = 200.0
 	if velocity.x < 0.0:
+		facing_right = false
 		scale.x = scale.y * -1
 		wall_x_force = -200.0
 
 func jump_logic():
 	if is_on_floor():
-		jump_amount = 2
-	
+		dash_number = 1
 	if Input.is_action_just_pressed("ui_accept"):
 		jump_amount -= 1
 		velocity.y -= lerp(jump_speed, acceleration, 0.1)
@@ -87,3 +104,24 @@ func wall_jumping():
 	await get_tree().create_timer(0.12).timeout
 	is_wall_jumping = false
 				
+
+func dash():
+	if dash_key_pressed == 1:
+		is_dashing = true
+	else:
+		is_dashing = false
+	if facing_right == true:
+		velocity.x= dash_speed
+		dash_started()
+	if facing_right == false:
+		velocity.x = -dash_speed
+		dash_started()
+		
+func dash_started():
+	if is_dashing == true:
+		dash_key_pressed = 1
+		await get_tree().create_timer(0.16).timeout
+		is_dashing = false
+		dash_key_pressed = 0
+	else:
+		return
